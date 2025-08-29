8dd3e7a273d7edc5fb1ebe709dad6d32c530556e
Van-D3r <farrel.hardiansyah@binus.ac.id>
2025-08-29 19:54:05 +0700

    tugas

---
+++ tugas1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "Farrel Ivander Hardiansyah-2902670023 bahasa pemrograman yg sedang saya pelajari dan yg akan saya kuasa yaitu c++/cpp" << endl;
+    return 0;
+}
