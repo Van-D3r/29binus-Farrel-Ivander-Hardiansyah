4408f374440b323d2a72b29e50816d5cacda030e
Van-D3r <farrel.hardiansyah@binus.ac.id>
2025-09-18 13:51:13 +0700

    belum selesai

---
+++ Problem/ProblemB.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int name;
+    name = "Bibi Jojo";
+    printf("Congrats",name "for joining the CS department at BINUS\n");
+    return 0;
+}
