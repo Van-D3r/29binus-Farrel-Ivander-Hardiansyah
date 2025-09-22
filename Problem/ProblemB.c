5d0b0bf22ed4144a5c3bddfd33a95abff36e059d
Van-D3r <farrel.hardiansyah@binus.ac.id>
2025-09-22 10:06:24 +0700

    done bree

--- Problem/ProblemB.c
+++ Problem/ProblemB.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main() {
-  
-  return 0;
+    char name[101]; // Menampung nama hingga 100 karakter + null terminator
+    fgets(name, sizeof(name), stdin); // Membaca input termasuk spasi
+
+    // Menghapus newline di akhir string jika ada
+    int i = 0;
+    while (name[i] != '\0') {
+        if (name[i] == '\n') {
+            name[i] = '\0';
+            break;
+        }
+        i++;
+    }
+
+    printf("Congrats \"%s\" for joining the CS department at BINUS.\n", name);
+
+    return 0;
 }
