7fbdf22bcfd29f1051826e5dab808540040e7940
Van-D3r <farrel.hardiansyah@binus.ac.id>
2025-09-30 15:26:15 +0700

    D

---
+++ Problem/ProblemDselection.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    for (int i = 1; i <= T; i++) {
+        int A, B, C;
+        scanf("%d %d %d", &A, &B, &C);
+
+        // Hitung cashback: (B% dari A)
+        int cashback = (A * B) / 100;
+
+        // Jika cashback lebih besar dari maksimum, pakai maksimum
+        if (cashback > C) {
+            cashback = C;
+        }
+
+        // Cetak hasil
+        printf("Case #%d: %d\n", i, cashback);
+    }
+
+    return 0;
+}
