4ef9b8d7c94892bb6efd6f90773842f34d4b4f7c
Van-D3r <farrel.hardiansyah@binus.ac.id>
2025-10-27 12:04:29 +0700

    lanjut

---
+++ cobaa/1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int T;
+    scanf("%d", &T);
+    // kiri = fungsinya dari mana
+    // tgah = mau berakhir di angka berapa
+    //kanan = dengan cara apa dari kiri ke kanan
+    for(int i = 0; i < T; i++){
+        printf("hallo ");
+    }
+    
+    return 0;
+}
