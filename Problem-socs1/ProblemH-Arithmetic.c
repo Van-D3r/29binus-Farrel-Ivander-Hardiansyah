b5f386578633b99d4eb1ae81b39ecc38de1788d5
Van-D3r <farrel.hardiansyah@binus.ac.id>
2025-11-06 11:06:38 +0700

    lanjut

--- Problem-socs1/ProblemH-Arithmetic.c
+++ Problem-socs1/ProblemH-Arithmetic.c
@@ -5,9 +5,13 @@ int main(){
 
     float total;
     total =(damage + megical + pure) / 3.0;
+
+    if (total < 0) total = 0;
+    if (total > 100) total = 100;
+
     printf("%.2f",total);
 
-
+    
 
 
 
