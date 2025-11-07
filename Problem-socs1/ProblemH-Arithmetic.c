ea9cb60c1f8c8fadff7907ccbc9661b247e1c0ee
Van-D3r <farrel.hardiansyah@binus.ac.id>
2025-11-07 13:40:18 +0700

    lanjut

--- Problem-socs1/ProblemH-Arithmetic.c
+++ Problem-socs1/ProblemH-Arithmetic.c
@@ -2,8 +2,11 @@
 int main(){
     int damage, megical, pure;
     scanf("%d %d %d",&damage,&megical,&pure);
-    
+
     double total = (damage * 0.20) + (megical * 0.30) + (pure * 0.50);
+    
+    if (total < 0) total = 0;
+    if (total > 100) total = 100;
 
     printf("%.2f",total);
 
