ff76e097addc4106950875ca605f7208cd569746
Van-D3r <farrel.hardiansyah@binus.ac.id>
2025-11-14 07:19:41 +0700

    hapus dikit

--- Problem-socs1/ProblemH-Arithmetic.c
+++ Problem-socs1/ProblemH-Arithmetic.c
@@ -4,9 +4,7 @@ int main(){
     scanf("%d %d %d",&damage,&megical,&pure);
 
     double total = (damage * 0.2) + (megical * 0.3) + (pure * 0.5);
-    
-    if (total < 0) total = 0;
-    if (total > 100) total = 100;
+
 
     printf("%.2f",total);
 
